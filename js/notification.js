f866e3ff1ce77bbe50758057f5c2fa8f0e71d119
Shankar Krishnasamy <shan_krishnasamy@yahoo.co.in>
2022-04-02 21:44:38 +0530

    EOD Update for Notification

--- js/notification.js
+++ js/notification.js
@@ -1,8 +1,13 @@
 function showNotifications() {
+
   var x = document.getElementById('notiAlert');
+  var bg = document.getElementById('notiBg');
+
   if (window.getComputedStyle(x, null).getPropertyValue("display") === 'none') {
     x.style.display = 'block';
+    bg.style.display = 'block';
   } else {
     x.style.display = 'none';
+    bg.style.display = 'none';
   }
 }
